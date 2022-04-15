753e7c77e6899341cae0fe35d174675e991da2e0
yosei_ito <yosei.i@latona.io>
2022-04-15 18:32:08 +0900

    added skeleton for production

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,17 @@
 FROM python:3
 ENV PYTHONDONTWRITEBYTECODE=1
 ENV PYTHONUNBUFFERED=1
+COPY requirements.txt /code/requirements.txt
+COPY . /code/
 WORKDIR /code
-ADD requirements.txt /code/requirements.txt
-RUN pip3 install -r requirements.txt
-ADD . /code/
+EXPOSE 8000
+RUN RUN pip3 install -r requirements.txt && \
+    adduser --disabled-password --no-create-home app &&\
+    mkdir -p /vol/web/static && \
+    mkdir -p /vol/web/media && \
+    chown -R app:app /vol && \
+    chown -R 755 /vol
+
+USER app
+
+CMD ["run.sh"]
--- ckintai_kun/settings.py
+++ ckintai_kun/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +21,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dtee-69bt0ruhqw_06kzev=&-5(5&z3c+e==b*maa0=z_v+u1m'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
-ALLOWED_HOSTS = ['192.168.100.230']
+ALLOWED_HOSTS = []
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.environ.get('ALLOWED_HOSTS', '').split(','),
+    )
+)
 
 
 # Application definition
