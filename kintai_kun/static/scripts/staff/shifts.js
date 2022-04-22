9db907656d9837f2de02cd30af0ea1a851cd3837
M.Lee <mengheng.l@latona.io>
2022-04-22 17:13:49 +0900

    added frontend of form submit

--- kintai_kun/static/scripts/staff/shifts.js
+++ kintai_kun/static/scripts/staff/shifts.js
@@ -2,8 +2,20 @@ window.onload = init
 
 function init(){
   $('#main_cb').on('change', (e) => {check_all(e)})
+  $('#shifts_form').on('submit', (e) => {confirm_form(e)})
 }
 
 function check_all(e){
   $(`input[data-group=${e.target.dataset['group']}]`).prop('checked', e.target.checked)
+}
+
+function confirm_form(e){
+  shifts_count = $('input:checkbox:checked[name="shifts[]"]').length
+  e.preventDefault()
+  if (shifts_count < 1){
+    alert('変更・削除項目を選択してください')
+  }
+  else if (confirm(`${shifts_count}項目を反映させますか`)){
+    e.target.submit()
+  }
 }
