37c4c0be71a1fc71bd6bd079be02258e3ec00166
yosei_ito <yosei.i@latona.io>
2022-04-22 10:49:16 +0900

    add makemigration to run.sh script

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,6 +3,7 @@
 set -e
 
 python manage.py collectstatic --noinput
+python manage.py makemigrations
 python manage.py migrate
 
 uwsgi --socket :9000 --workers 4 --master --enable-threads --module ckintai_kun.wsgi --uid 1000 --gid 1000
