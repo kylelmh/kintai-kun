f0d080f1e7ca306ddf81353ffc5190f4a5f44f8f
M.Lee <mengheng.l@latona.io>
2022-04-23 17:15:26 +0900

    add month filtering for dakoku

--- kintai_kun/views/main/dakoku.py
+++ kintai_kun/views/main/dakoku.py
@@ -2,12 +2,22 @@ from kintai_kun.views.custom_views import UserView
 from django.http import HttpResponse
 from kintai_kun.models import WorkTimestamp
 from django.shortcuts import render
+from django.utils import timezone
 import json
 
 class DakokuView(UserView):
   def get(self, request, *args, **kwargs):
+    month = request.GET.get('month')
+    if not month:
+      month = timezone.now().month
+    timestamps = WorkTimestamp.objects.filter(
+      employee = request.user.employee,
+      created_on__year = timezone.now().year,
+      created_on__month = month
+    ).order_by('-created_on')
     context = { 
-      'timestamps': request.user.employee.worktimestamp_set.order_by('-created_on')[:40]
+      'timestamps': timestamps,
+      'month': month,
     }
     return render(request, 'main/index.html', context=context)
 
--- kintai_kun/views/staff/dakoku.py
+++ kintai_kun/views/staff/dakoku.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from kintai_kun.views.custom_views import StaffView
 from kintai_kun.models import WorkTimestamp
+from django.db.models import Q
 from django.core.paginator import Paginator
 
 class StaffDakokuView(StaffView):
