fe36c59f16709eee4fd2833c5159e3e58962b5d4
M.Lee <mengheng.l@latona.io>
2022-05-13 17:53:46 +0900

    added employees table

--- kintai_kun/views/staff/employees.py
+++ kintai_kun/views/staff/employees.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render, redirect
 
 from kintai_kun.views.custom_views import StaffView
+from kintai_kun.models import Employee
 from django.contrib import messages
 from django.core.paginator import Paginator
 from django.utils import timezone
 
+
 class StaffEmployeesView(StaffView):
   def get(self, request):
-    return render(request, 'staff/employees/index.html')
+    employees = Employee.objects.all().order_by('user__last_name')
+    page_number = request.GET.get('page')
+    context = {
+      'employees' : Paginator(employees, 30).get_page(page_number)
+    }
+    return render(request, 'staff/employees/index.html', context=context)
