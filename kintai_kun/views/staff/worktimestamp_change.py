ad7846fff80d85ae68c46e26f406a02c282afd9f
M.Lee <mengheng.l@latona.io>
2022-05-20 16:07:03 +0900

    added boilerplate for staff page

--- kintai_kun/views/staff/worktimestamp_change.py
+++ kintai_kun/views/staff/worktimestamp_change.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponseNotAllowed
 
 from kintai_kun.views.custom_views import StaffView
-from kintai_kun.models import WorkTimestamp
+from kintai_kun.models import WorkTimestamp, WorkTimestampChange
 from kintai_kun.forms import StaffShiftForm
 from django.contrib import messages
 from django.core.paginator import Paginator
@@ -12,7 +12,10 @@ from django.db import transaction
 
 class StaffWTChangeView(StaffView):
   def get(self, request):
-    return render(request, 'staff/worktimestamp_change/index.html')
+    context = {
+      'wt_changes': WorkTimestampChange.objects.order_by('approving_employee', '-created_on')
+    }
+    return render(request, 'staff/worktimestamp_change/index.html', context=context)
 
   @transaction.atomic
   def post(self, request):
