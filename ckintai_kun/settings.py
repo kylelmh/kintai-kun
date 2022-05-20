5f403f7bace00fe9c638e49936720c45a3fb7620
M.Lee <mengheng.l@latona.io>
2022-05-20 13:37:38 +0900

    changed redirect url to root /

--- ckintai_kun/settings.py
+++ ckintai_kun/settings.py
@@ -145,7 +145,7 @@ STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = '/dakoku'
+LOGIN_REDIRECT_URL = '/'
 LOGIN_URL='/login'
 LOGOUT_REDIRECT_URL = '/login'
 
