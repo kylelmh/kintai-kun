ee1115c93fea9c5e0a140b59dfcf76db3bbc526d
Kyle Lee <kyle.lhe@gmail.com>
2022-05-24 09:58:37 +0900

    WIP: added vite django package

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'vite/modulepreload-polyfill'
 
 createApp(App).mount('#app')
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,15 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/static/static/',
   server: {
-    host: 'localhost'
+    host: '0.0.0.0'
+  },
+  build: {
+    manifest: true,
+    rollupOptions: {
+      input: {asset: '/vite/src/main.js'}
+    },
+    outDir: '/vite/dist'
   }
 })
