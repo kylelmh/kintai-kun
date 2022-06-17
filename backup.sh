80f25322805085a83f551fe550bd784a06120809
kylelmh <kyle.lhe@gmail.com>
2022-06-17 17:44:37 +0900

    minor fixes

--- backup.sh
+++ backup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+cd /home/latona/deploy/kintai-kun
 rm /home/latona/deploy/dumps/*.dump
 now=$(date +"%Y%m%d")
 year=$(date +"%Y")
 month=$(date +"%m")
 echo "Dumping postgres database..."
-docker-compose exec db pg_dump postgres > /home/latona/deploy/dumps/${now}.dump
+docker-compose exec db pg_dump postgres -F c > /home/latona/deploy/dumps/${now}.dump
 echo "Syncing with Google Drive"
 rclone copy /home/latona/deploy/dumps/ gdrive:kintai_dumps/${year}/${month}/
